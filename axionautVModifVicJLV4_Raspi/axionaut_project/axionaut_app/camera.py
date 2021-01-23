3c6881953a60555c776f7ff04281f3eae63604e1
Victor Duleba <victor.duleba@gmail.com>
2021-01-23 17:42:53 +0100

    victor

--- axionautVModifVicJLV4_Raspi/axionaut_project/axionaut_app/camera.py
+++ axionautVModifVicJLV4_Raspi/axionaut_project/axionaut_app/camera.py
@@ -28,13 +28,13 @@ class Camera(object):
     def _thread(cls):
         with picamera.PiCamera() as camera:
             # camera setup
-            camera.resolution = (250, 130)
+            camera.resolution = (320, 240)
             #camera.hflip = True
             #camera.vflip = True
 
             # let camera warm up
-            #camera.start_preview()
-            #time.sleep(2)
+            # camera.start_preview()
+            # time.sleep(2)
 
             stream = io.BytesIO()
             for foo in camera.capture_continuous(stream, 'jpeg',

ecedceca7c84d66af15c9b25af294c2d87a9083e
SMNJSPHHRV <simon.herve@edu.devinci.fr>
2021-02-09 07:57:51 +0100

    version PC

---
+++ PourVictor/axionaut_project/axionaut_app/test_pwm.py
@@ -0,0 +1,18 @@
+import Adafruit_PCA9685
+import time
+
+servo = Adafruit_PCA9685.PCA9685()
+servo.set_pwm_freq(60)
+
+while True: 
+    print("True")
+    servo.set_pwm(0,0,423)
+    time.sleep(1)
+    servo.set_pwm(0,0,386)
+    time.sleep(1)
+    servo.set_pwm(0,0,350)
+    time.sleep(1)
+    servo.set_pwm(0,0,313)
+    time.sleep(1)
+    servo.set_pwm(0,0,276)
+    time.sleep(1)
